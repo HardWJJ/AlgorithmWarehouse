1ca8aa91994bea2375dd215020a556c11ac606b2
wjiajun <wjiajun@linewell.com>
2019-08-14 09:04:05 +0800

    算法代码

---
+++ src/main/java/com/github/hardwjj/swordoffer/s18z2/Solution.java
@@ -0,0 +1,19 @@
+package com.github.hardwjj.swordoffer.s18z2;
+
+
+public class Solution {
+
+    public ListNode deleteDuplication(ListNode pHead) {
+
+    }
+
+}
+
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
