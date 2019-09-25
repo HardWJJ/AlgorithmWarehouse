6737cd9811a5490444f60265092d955c804eb35a
wjiajun <wjiajun@linewell.com>
2019-09-25 09:29:49 +0800

    算法代码

--- src/main/java/com/github/hardwjj/swordoffer/s61/Solution.java
+++ src/main/java/com/github/hardwjj/swordoffer/s61/Solution.java
@@ -2,6 +2,14 @@ package com.github.hardwjj.swordoffer.s61;
 
 import java.util.Arrays;
 
+/**
+ * 题：扑克牌顺子
+ *
+ * 五张牌，其中大小鬼为癞子，牌面为 0。判断这五张牌是否能组成顺子。
+ *
+ * 解题思路：
+ *  使用动态规划，利用无后效性的特性将空间复杂度缩小为O(N)
+ */
 public class Solution {
     public boolean isContinuous(int [] numbers) {
         if(numbers == null || numbers.length == 0){
@@ -18,11 +26,13 @@ public class Solution {
             }
         }
 
+        // 由于之前排了序所以癞子排都在最前面
         for (int i = cnt; i < numbers.length - 1; i++) {
             // 无法通过插入成为顺子
             if(numbers[i + 1] == numbers[i]){
                 return false;
             }
+            // 多减一保证顺序不减掉癞子的牌
             cnt -= (numbers[i + 1] - numbers[i]) - 1;
         }
         // 如果可抵消牌被用完或者有剩余
