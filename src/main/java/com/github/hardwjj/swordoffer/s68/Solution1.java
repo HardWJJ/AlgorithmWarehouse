a6f6d173bf384a1c1e5a349ab9a1713c2a5d89c2
wjiajun <wjiajun@linewell.com>
2019-10-09 09:28:08 +0800

    算法代码

--- src/main/java/com/github/hardwjj/swordoffer/s68/Solution1.java
+++ src/main/java/com/github/hardwjj/swordoffer/s68/Solution1.java
@@ -2,13 +2,12 @@ package com.github.hardwjj.swordoffer.s68;
 
 
 /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
+ * 题：树中两个节点的最低公共祖先（二叉查找树）
+ *
+ * 二叉查找树中，两个节点 p, q 的公共祖先 root 满足 root.val >= p.val && root.val <= q.val。
+ *
+ * 解题思路：
+ *  递归求解，根据左右节点的比较值方向进行搜索
  */
 public class Solution1 {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
@@ -16,10 +15,12 @@ public class Solution1 {
             return null;
         }
 
+        // 左右节点都小于，往左走
         if(root.val > p.val  && root.val > q.val) {
             lowestCommonAncestor(root.left, p, q);
         }
 
+        // 左右节点都大于，往右走
         if(root.val < p.val && root.val < q.val){
             lowestCommonAncestor(root.right, p ,q);
         }
