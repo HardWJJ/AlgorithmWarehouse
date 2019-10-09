51ed816126bb3784ebf67d7e6ec03e6f61f8aac5
wjiajun <657447542@qq.com>
2019-10-09 09:28:08 +0800

    算法代码

--- src/main/java/com/github/hardwjj/swordoffer/s67/Solution.java
+++ src/main/java/com/github/hardwjj/swordoffer/s67/Solution.java
@@ -1,7 +1,24 @@
 package com.github.hardwjj.swordoffer.s67;
 
+/**
+ * 题：把字符串转换成整数
+ *
+ * 将一个字符串转换成一个整数，字符串不是一个合法的数值则返回 0，要求不能使用字符串转换整数的库函数。
+ *
+ * Iuput:
+ *  +2147483647
+ *  1a33
+ *
+ * Output:
+ *  2147483647
+ *  0
+ *
+ * 解题思路：
+ *      从高位到低位逐个遍历，高位*10 + (低位-'0')，类似递归后半步骤，自底向上，刚才开始还需要判断数的正负性
+ */
 public class Solution {
     public int StrToInt(String str) {
+        // 判空
         if(str == null || str.length() == 0){
             return 0;
         }
@@ -10,8 +27,10 @@ public class Solution {
 
         int ret = 0;
 
+        // 遍历数组
         for (int i = 0; i < str.toCharArray().length; i++) {
-            if(i == 0 && str.charAt(i) == '-' || str.charAt(i) == '+') {
+            // 判断首个字符的正负性
+            if(i == 0 && (str.charAt(i) == '-' || str.charAt(i) == '+')){
                 continue;
             }
 
--- src/main/java/com/github/hardwjj/swordoffer/s68/Solution2.java
+++ src/main/java/com/github/hardwjj/swordoffer/s68/Solution2.java
@@ -1,10 +1,16 @@
 package com.github.hardwjj.swordoffer.s68;
 
 /**
- * @author wjiajun
+ * 题：树中两个节点的最低公共祖先（二叉查找树）
+ *
+ * 二叉查找树中，两个节点 p, q 的公共祖先 root 满足 root.val >= p.val && root.val <= q.val。
+ *
+ * 解题思路：
+ *  递归求解，根据左右节点的比较值方向进行搜索
  */
 public class Solution2 {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // 如果root节点等于左右的一个节点，返回root节点
         if(root == null || p == root || q == root){
             return root;
         }
